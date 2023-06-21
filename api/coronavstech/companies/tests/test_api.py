d227763d9051e21b141799458a5a52c6be4aef90
badx86 <badlolpro@gmail.com>
2023-06-21 05:13:28 +0400

    add api tests

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+import logging
 from unittest import TestCase
 from django.test import Client
 from django.urls import reverse
@@ -81,9 +82,40 @@ class TestPostCompanies(BasicCompanyAPiTestCase):
         self.assertIn("is not a valid choice", str(response.content))
 
     @pytest.mark.xfail
-    def test_should_be_ok_if_fails(self)->None:
+    def test_should_be_ok_if_fails(self) -> None:
         self.assertEqual(1, 2)
 
     @pytest.mark.skip
-    def test_should_be_skipped(self)->None:
+    def test_should_be_skipped(self) -> None:
         self.assertEqual(1, 2)
+
+
+def raise_covid19_exception() -> None:
+    raise ValueError("CoronaVirus Exception")
+
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert "CoronaVirus Exception" == str(e.value)
+
+
+logger = logging.getLogger("CORONA_LOGS")
+
+
+def function_that_logs_something() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.warning(f"I am logging {str(e)}")
+
+
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs_something()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info("I am logging info level")
+        assert "I am logging info level" in caplog.text
