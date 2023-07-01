a36ba8181787abaf29a91649e211d4168e835dca
Badx86 <Badx86@users.noreply.github.com>
2023-07-01 23:16:41 +0000

    Automated deployment: Sat Jul  1 23:16:41 UTC 2023 56f89cb5908e220c2d71bef3c6096ee7f5ebfec0

---
+++ fibonacci/conftest.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from typing import Callable
+from dotenv import load_dotenv
+from rest_framework.test import APIClient
+import pytest
+
+
+@pytest.fixture
+def time_tracker():
+    tick = datetime.now()
+    yield
+    tock = datetime.now()
+    diff = tock - tick
+    print(f"\n runtime: {diff.total_seconds()}")
+
+
+class PerformanceException(Exception):
+    def __init__(self, runtime: timedelta, limit: timedelta):
+        self.runtime = runtime
+        self.limit = limit
+
+
+def track_performance(method: Callable, runtime_limit=timedelta(seconds=2)):
+    def run_function_and_validate_runtime(*args, **kwargs):
+        tick = datetime.now()
+        result = method(*args, **kwargs)
+        method(*args, **kwargs)
+        tock = datetime.now()
+        runtime = tock - tick
+        print(f"\n runtime: {runtime.total_seconds()}")
+
+        if runtime > runtime_limit:
+            raise PerformanceException(runtime=runtime, limit=runtime_limit)
+        return result
+
+    return run_function_and_validate_runtime
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
---
+++ fibonacci/tests/test_performance.py
@@ -0,0 +1,19 @@
+import pytest
+from fibonacci.dynamic import fibonacci_dynamic_v2
+from fibonacci.conftest import track_performance
+
+
+@pytest.mark.performance
+@track_performance
+def test_performance():
+    fibonacci_dynamic_v2(1000)
+
+
+@pytest.mark.performance
+def test_performance_under_load():
+    for _ in range(2000):
+        @track_performance
+        def run_test():
+            fibonacci_dynamic_v2(1000)
+
+        run_test()
